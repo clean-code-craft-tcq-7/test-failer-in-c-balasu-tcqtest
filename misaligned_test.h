83267f804e48a1dd5a860fc1363f34d4b20c8126
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-11-27 19:36:04 +0530

    Update misaligned_test.h

--- misaligned_test.h
+++ misaligned_test.h
@@ -1,8 +1,8 @@
 typedef struct
 {
     int pairNum;
-    char* majorColor;
-    char* minorColor;
+    const char* majorColor;
+    const char* minorColor;
 }colorPairInfo;
 
 #define MAX_COLOR_PAIR_POSSIBLE 25
