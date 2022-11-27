bfd5614c84fa05133b437f64ceb693d26bb7f47a
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-11-27 19:48:09 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -7,9 +7,11 @@
 colorPairInfo testColorPair[MAX_COLOR_PAIR_POSSIBLE];
 #endif
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
     int i = 0, j = 0, testIdx = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
@@ -28,9 +30,12 @@ int main() {
     int result = printColorMap();
     assert(result == 25);
     // Similary all pairs shall be evaluated using the testColorPair variable
-    assert(testColorPair[0].pairNum == 1);
+    //assert(testColorPair[0].pairNum == 1);
     assert(strcmp(testColorPair[0].majorColor, "White") == 0);
     assert(strcmp(testColorPair[0].majorColor, "Blue") == 0);
+    
+    assert(strcmp(testColorPair[0].majorColor, "White") == 0);
+    assert(strcmp(testColorPair[0].majorColor, "Orange") == 0);
     
     printf("All is well (maybe!)\n");
     return 0;
